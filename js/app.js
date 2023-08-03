7c7ea6c5f60f5bc68e0c2adb69a26f6ee0129d6b
AriArch <rogerfiblasalvado@gmail.com>
2023-08-04 00:51:19 +0200

    start projects

--- js/app.js
+++ js/app.js
@@ -10,3 +10,28 @@ toggleBtn.onclick = () => {
         ? 'fa-solid fa-xmark'
         : 'fa-solid fa-bars'
 }
+
+
+
+
+
+let open = 'fa-angles-down';
+let close = 'fa-xmark';
+
+const pythonArr = document.querySelector('.pc-python i');
+const pythonContent = document.querySelector('.pc-python div');
+
+pythonArr.addEventListener('click', () => {
+    if (pythonArr.classList.contains(close)) {
+        pythonArr.classList.remove(close);
+        pythonArr.classList.add(open);
+
+        pythonContent.style.display = 'none';
+    } else {
+        pythonArr.classList.remove(open);
+        pythonArr.classList.add(close);
+
+        pythonContent.style.display = 'block';
+
+    }
+})
