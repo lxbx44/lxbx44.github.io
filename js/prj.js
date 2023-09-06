fc8b756b3c4d1f300559405dba3883c0008e480c
FedoraAria <rogerfiblasalvado@gmail.com>
2023-09-06 20:38:28 +0200

    update prj

---
+++ js/prj.js
@@ -0,0 +1,17 @@
+
+const copyElements = document.querySelectorAll('.fa-copy');
+
+copyElements.forEach((cpyBtn) => {
+    cpyBtn.addEventListener('click', () => {
+        let cpyContent = cpyBtn.parentElement.querySelector('p span');
+        navigator.clipboard.writeText(cpyContent.innerHTML);
+
+        // Animation popup
+
+        const popup = document.querySelector('#copyPopup');
+        
+        popup.style.display = 'block';
+        popup.style.animation = 'copyPopup 2s'
+        popup.style.display = 'none';
+    })
+})
